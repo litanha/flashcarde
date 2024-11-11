6b4367a5f043d047e590c34f4054007c1b239fe1
litanha <141042264+litanha@users.noreply.github.com>
2024-11-11 10:59:48 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+ function criaCartao(categoria, pergunta, resposta) {
+  let container = document.getElementById('container')
+  letcartao = document.createElement('article')
+  cartao.className = 'cartao'
+}
+ cratao.innerHTML= `
+<div class="cartao__conteudo">
+<h3>geografia</h3>
+<div class="cartao__conteudo__pergunta">
+<p>qual é a capital da frança?</p> 
+</div>
+<div class="cartao__conteudo__resposta">
+<p> O JavaScript é uma linguagem de programação</p>
+ </div>
+ </div>
+               `
---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'programação',
+    'o que é JavaScript?',
+   'o JavaScript é uma linguagem de programação.'
+    
+)
+
+criaCartao(
+    'geografia',
+     'qual a capital da França?',
+     'A capital da frança é Paris.'
+)
+
+criaCartao(
+    'programação',
+    'oque é uma função?',
+    'Uma função é um bloco de código que executa uma tarefa.'
+)
